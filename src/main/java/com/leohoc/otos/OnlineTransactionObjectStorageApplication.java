877a2ddb4ee24ccd51d1283b9aaaa60f69c2acd0
Leonardo Carvalho <leohoc@gmail.com>
2020-08-08 14:13:55 -0300

    first commit

---
+++ src/main/java/com/leohoc/otos/OnlineTransactionObjectStorageApplication.java
@@ -0,0 +1,13 @@
+package com.leohoc.otos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTransactionObjectStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTransactionObjectStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leohoc/otos/OnlineTransactionObjectStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leohoc.otos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTransactionObjectStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
